fb9d9e9bee1b5f6d4becb09fcf1a9b068d4d8881
onasahmed <ismamahmed777@gmail.com>
2026-08-30 12:37:41 +0600

    cors change

--- index.js
+++ index.js
@@ -19,7 +19,22 @@ const searchRoutes = require("./src/routes/search.routes");
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:3000",
+  "https://what-u-find.netlify.app"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
